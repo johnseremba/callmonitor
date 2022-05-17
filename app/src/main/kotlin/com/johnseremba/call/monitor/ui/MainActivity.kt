7fdcae36f9298ba19c7c5dc07ed3cb6b3ba04c93
John Seremba <john.seremba@sumup.com>
2022-05-18 00:33:04 +0200

    Create Call monitor layout files

--- app/src/main/kotlin/com/johnseremba/call/monitor/ui/MainActivity.kt
+++ app/src/main/kotlin/com/johnseremba/call/monitor/ui/MainActivity.kt
@@ -2,12 +2,15 @@ package com.johnseremba.call.monitor.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.johnseremba.call.monitor.R
+import com.johnseremba.call.monitor.databinding.ActivityMainBinding
 import com.johnseremba.call.monitor.di.AppKoinComponent
 
 class MainActivity : AppCompatActivity(), AppKoinComponent {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
